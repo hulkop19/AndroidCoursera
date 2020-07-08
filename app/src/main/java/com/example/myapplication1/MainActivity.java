e843f6a7e9c6ff74582785ce8106deede8f9d805
dima <hulkop14@gmail.com>
2020-07-08 23:51:08 +0300

    First commit

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("First kek1");
+        System.out.println("First kek12");
     }
 }
